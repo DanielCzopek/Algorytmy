5fcc5b11ba206620f144f762ccee253c109148c4
Daniel Czopek <Daniel.Czopek@microsoft.wsei.edu.pl>
2022-05-31 11:02:39 +0200

    zad 2 działa

--- Lab_13/Program.cs
+++ Lab_13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab_13
 {
@@ -47,13 +48,47 @@ namespace Lab_13
         //czy input jest palindromem
         public static bool IsPalindrome(string input)
         {
-            return false;
+            if (String.IsNullOrEmpty(input))
+                return false;
+            input = input
+                          .Replace(" ", "")
+                          .ToLower();
+
+            int length = input.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (input[i] != input[length - i - 1])
+                    return false;
+            }
+            return true;
         }
 
         //czy łańcuchy są anagramami
         public static bool IsAnagrams(string a, string b)
         {
-            return false;
+            if (a.Length != b.Length)
+                return false;
+
+            if (a == b)
+                return true;//or false: Don't know whether a string counts as an anagram of itself
+
+            Dictionary<char, int> pool = new Dictionary<char, int>();
+            foreach (char element in a.ToCharArray()) //fill the dictionary with that available chars and count them up
+            {
+                if (pool.ContainsKey(element))
+                    pool[element]++;
+                else
+                    pool.Add(element, 1);
+            }
+            foreach (char element in b.ToCharArray()) //take them out again
+            {
+                if (!pool.ContainsKey(element)) //if a char isn't there at all; we're out
+                    return false;
+                if (--pool[element] == 0) //if a count is less than zero after decrement; we're out
+                    pool.Remove(element);
+            }
+            return pool.Count == 0;
         }
 
 
