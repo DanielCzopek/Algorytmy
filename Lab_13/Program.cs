ed0f296113f6a66b12e6ccace6c071898789fb71
Daniel Czopek <Daniel.Czopek@microsoft.wsei.edu.pl>
2022-05-31 10:55:52 +0200

    przekopiowano plik z polecenia

--- Lab_13/Program.cs
+++ Lab_13/Program.cs
@@ -6,7 +6,64 @@ namespace Lab_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int points = 0;
+            if (
+                IsPalindrome("a")
+                && IsPalindrome("aaa")
+                && IsPalindrome("")
+                && IsPalindrome("zakaz")
+                && IsPalindrome("ZaKAZ")
+                && IsPalindrome("KamilŚlimak")
+                && !IsPalindrome("abc")
+                )
+            {
+                Console.WriteLine("Zadanie 1: ok");
+                points += 1;
+            }
+            if (
+                IsAnagrams("abcd", "bcda")
+                && IsAnagrams("aa", "aa")
+                && !IsAnagrams("AA", "aa")
+                && IsAnagrams("", "")
+                && !IsAnagrams("abc", "abca")
+                )
+            {
+                Console.WriteLine("Zadanie 2: ok");
+                points += 1;
+            }
+            if (
+                LongestIdenticalString("aaaa").Equals("aaaa")
+                && LongestIdenticalString("abcddddaaddd").Equals("dddd")
+                && LongestIdenticalString("abcd").Equals("a")
+                && LongestIdenticalString("abbcdd").Equals("bb")
+                )
+            {
+                Console.WriteLine("Zadanie 3: ok");
+                points += 2;
+            }
         }
+
+
+        //czy input jest palindromem
+        public static bool IsPalindrome(string input)
+        {
+            return false;
+        }
+
+        //czy łańcuchy są anagramami
+        public static bool IsAnagrams(string a, string b)
+        {
+            return false;
+        }
+
+
+        //zwróć pierwszy najdłuższy fragment złożony z powtarzających się znaków wejścia
+        public static string LongestIdenticalString(string input)
+        {
+            return "";
+        }
+
     }
 }
+
+
